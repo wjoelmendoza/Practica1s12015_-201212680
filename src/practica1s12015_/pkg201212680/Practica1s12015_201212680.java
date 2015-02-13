6ba4a1cb2e6344254c875886d321899336621968
Walter Mendoza <Walter Mendoza@PowerMachine>
2015-02-12 22:02:31 -0600

    Primer commit  solo de sincronizacion con Github

---
+++ src/practica1s12015_/pkg201212680/Practica1s12015_201212680.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package practica1s12015_.pkg201212680;
+
+/**
+ *
+ * @author Walter Mendoza
+ */
+public class Practica1s12015_201212680 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
